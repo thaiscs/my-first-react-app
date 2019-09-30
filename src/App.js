86a6cfb8e5bfad290eab3906685e32e938c27054
thaiscs <thais.advj@gmail.com>
2019-09-30 11:59:37 +0200

    welcome to Codaisseur added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          LET`S LEARN REACT TODAY Yaaay
+          Welcome to Codaisseur
         </p>
         <a
           className="App-link"
